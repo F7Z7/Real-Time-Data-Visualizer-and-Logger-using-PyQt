c1f67191e330ac308d5a3702c40a503f673d13fa
farza <farzanrichu610@gmail.com>
2025-06-07 22:29:00 +0530

    basic entry point set

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print("this is the entry point")
-print("installed dependencies")
+import sys #for accesing system
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Real-Time Data Visualizer")
+        self.setGeometry(700,300,400,400)
+        pass
+
+def main():
+    app =QApplication(sys.argv) #use command line arguments
+    window = MainWindow() #instance of class
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
