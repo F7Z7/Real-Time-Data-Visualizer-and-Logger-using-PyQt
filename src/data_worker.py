f3345e860d3b02f0a0e11f14d6d16edd6fb81dd3
farza <farzanrichu610@gmail.com>
2025-06-14 03:13:20 +0530

    updated data_worker.py for versatality in using signals

--- src/data_worker.py
+++ src/data_worker.py
@@ -1,25 +1,45 @@
 from PyQt5.QtCore import QObject, pyqtSignal
-from src.plotting import sine_graph,cos_graph
+from src.plotting import (
+    sine_graph, cos_graph, tan_graph, cot_graph,
+    cosec_graph, sec_graph, square_graph, triangle_graph
+)
 import time
 
 class DataWorker(QObject):
+    data_ready = pyqtSignal(float, float, float)  # t, y1, y2
 
-    data_ready=pyqtSignal([float,float,float]) #t,sine,cos
     def __init__(self, dt=0.05):
         super().__init__()
-        self.dt=dt #tim-step
-        self.running=False
-        self.t=0 #inital time
+        self.dt = dt  # time step
+        self.running = False
+        self.t = 0    # initial time
 
-    def start_work(self):
-        self.running=True
-        self.t=0
+        self.signal_map = {
+            "Sine": sine_graph,
+            "Cosine": cos_graph,
+            "Tangent": tan_graph,
+            "Cosecant": cosec_graph,
+            "Secant": sec_graph,
+            "Cotangent": cot_graph,
+            "Square": square_graph,
+            "Triangle": triangle_graph
+        }
+
+    def start_work(self, signal1, signal2):
+        self.running = True
+        self.t = 0
+
         while self.running:
-            sine_val=sine_graph(self.t)
-            cos_val=cos_graph(self.t)
-            self.data_ready.emit(self.t, sine_val, cos_val) #gives value to the ui
-            self.t+=self.dt
-            time.sleep(self.dt) #suspends action
+            func1 = self.signal_map.get(signal1, lambda t: 0) #find the correspodning signal from map
+            func2 = self.signal_map.get(signal2, lambda t: 0)
+
+            y1 = func1(self.t)
+            y2 = func2(self.t)
+
+            self.data_ready.emit(self.t, y1, y2)
+
+            self.t += self.dt
+            time.sleep(self.dt)
 
     def stop_work(self):
-        self.running=False
+        self.running = False
