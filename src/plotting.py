b8099f842ae6f3a657a47e6ca5cca79b88c9f83f
farza <farzanrichu610@gmail.com>
2025-06-15 01:13:51 +0530

    replaced math with numpy

--- src/plotting.py
+++ src/plotting.py
@@ -1,34 +1,30 @@
-import math
+import numpy as np
 
 safe_val=1e-5 #very small value to approx zero
 
 def safe_div(num,denom):
-    if abs(denom) > safe_val:
-        return num/denom
-    else:
-        return float('nan')
+    return np.divide(num, denom, out=np.full_like(num, np.nan), where=np.abs(denom) > safe_val)
+
 def sine_graph(t):
-    return math.sin(2 * math.pi * t)
+    return np.sin(2 * np.pi * t)
 
 def cos_graph(t):
-    return math.cos(2 * math.pi * t)
+    return np.cos(2 * np.pi * t)
 
 def tan_graph(t):
-    return safe_div(math.sin(2 * math.pi * t), math.cos(2 * math.pi * t))
+    return safe_div(np.sin(2 * np.pi * t), np.cos(2 * np.pi * t))
 
 def cosec_graph(t):
-    return safe_div(1, math.sin(2 * math.pi * t))
+    return safe_div(1, np.sin(2 * np.pi * t))
 
 def sec_graph(t):
-    return safe_div(1, math.cos(2 * math.pi * t))
+    return safe_div(1, np.cos(2 * np.pi * t))
 
 def cot_graph(t):
-    return safe_div(math.cos(2 * math.pi * t), math.sin(2 * math.pi * t))
+    return safe_div(np.cos(2 * np.pi * t), np.sin(2 * np.pi * t))
 
 def triangle_graph(t):
-
-    return 2 * abs(2 * (t % 1) - 1) - 1
+    return 2 * np.abs(2 * (t % 1) - 1) - 1
 
 def square_graph(t):
-
-    return 1 if (t % 1) < 0.5 else -1
+    return np.where((t % 1) < 0.5, 1, -1)
