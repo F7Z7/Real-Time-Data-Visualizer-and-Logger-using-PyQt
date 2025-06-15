cdb25cd6169c4f37751fccd1cc54506b142bfd73
farza <farzanrichu610@gmail.com>
2025-06-16 01:02:12 +0530

    .

--- src/math_functions.py
+++ src/math_functions.py
@@ -12,6 +12,24 @@ def get_signal_name(name):
         "Triangle": triangle_graph,
         "Square": square_graph
     }.get(name, None)
-
+operation_map = {
+    "A + B": lambda A, B: A + B,
+    "A - B": lambda A, B: A - B,
+    "A * B": lambda A, B: A * B,
+    "A / B": lambda A, B: A / B,
+    "sin(A)": lambda A, B: np.sin(A),
+    "cos(B)": lambda A, B: np.cos(B),
+    "sin(A) + 2*B": lambda A, B: np.sin(A) + 2 * B,
+}
 def compute_expression(t,inputA,inputB,operation,constants):
-    print("ji3")
+    signalA = get_signal_name(inputA)
+    signalB = get_signal_name(inputB)
+
+    A=signalA(t)
+    B=signalB(t)
+
+    operations=["+", "-", "*", "/", "sin", "cos", "A+B", "A*2", "sin(A) + 2*B"]
+    if not operation in operations:
+        print("Invalid operation")
+    else:
+        calculated_expression=operation_map.get(operation)
