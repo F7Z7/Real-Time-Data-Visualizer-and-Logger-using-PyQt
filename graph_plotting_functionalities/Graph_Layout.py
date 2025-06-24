3c72c9c6b103ba0d528a5ef8c195cf35cbfbd13c
farza <farzanrichu610@gmail.com>
2025-06-24 23:31:26 +0530

    Full layout set for dynamically including graphs

---
+++ graph_plotting_functionalities/Graph_Layout.py
@@ -0,0 +1,74 @@
+#here full structre is integrated
+#layout->input panle->graph stacked
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox
+from graph_plotting_functionalities.graph_widget import GraphWidget
+
+
+class Generate_Graph(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setMinimumSize(1000, 800)
+        self.graphs = []
+        self.dynamic_graphs_layout = QVBoxLayout()
+        self.initUI()
+
+    def initUI(self):
+
+        graph_layout = QVBoxLayout()
+        graph_layout.setSpacing(5)
+
+    #input grpah entering
+
+        sub_layout=QVBoxLayout()
+        sub_layout.addWidget(QLabel('Set the number of graphs'))
+        sub_layout.setAlignment(Qt.AlignTop)
+
+
+        input_layout = QHBoxLayout()
+        input_layout.addWidget(QLabel('Enter no of graphs'))
+        self.inupt_graphs = QLineEdit()
+        self.inupt_graphs.setPlaceholderText("e.g. 3")
+        input_layout.addWidget(self.inupt_graphs)
+
+        sub_layout.addLayout(input_layout)
+
+        self.set_btn = QPushButton('Set')
+        self.set_btn.clicked.connect(self.on_set_clicked)
+
+        sub_layout.addWidget(self.set_btn)
+
+        graph_layout.addLayout(sub_layout)
+
+
+        #here goes the input stack of graphs
+
+        self.dynamic_graph_layout = QVBoxLayout()
+        self.dynamic_graph_layout.setSpacing(5)
+
+        graph_layout.addLayout(self.dynamic_graph_layout)
+
+        self.setLayout(graph_layout)
+
+    def on_set_clicked(self):
+        try:
+            total_graphs=int(self.inupt_graphs.text())
+            if total_graphs <= 0:
+                QMessageBox.warning(self, "Error", "Please enter a positive number")
+                return
+
+            # clear graphs
+            for i in reversed(range(self.dynamic_graphs_layout.count())):
+                widget = self.dynamic_graphs_layout.itemAt(i).widget()
+                if widget:
+                    widget.setParent(None)
+
+            self.graphs = []
+            for i in range(total_graphs):
+                graph_widget = GraphWidget(graph_id=i, num=total_graphs)
+                self.graphs.append(graph_widget)
+                self.dynamic_graphs_layout.addWidget(graph_widget)
+
+        except ValueError:
+            QMessageBox.critical(self, "Error", "Invalid Value")
+
