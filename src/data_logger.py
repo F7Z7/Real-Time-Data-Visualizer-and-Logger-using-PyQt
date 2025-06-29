ec37d4910240f8e6c2faf410d4d444f1b4496b01
farza <farzanrichu610@gmail.com>
2025-06-30 03:13:29 +0530

    moved both functions inside a class DataLogger

--- src/data_logger.py
+++ src/data_logger.py
@@ -1,28 +1,37 @@
-import csv,os
-def logg_csv(graph_widget, directory: str,signal_name:str):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+import csv
+import os
 
-    try:
-        x_data,y_data = graph_widget.getData()
+class DataLogger:
+    def __init__(self, curve, signal_name, directory):
+        self.curve = curve
+        self.signal_name = signal_name
+        self.directory = directory
 
-    except AttributeError:
-        print(f"No such data is available in the curve for signal {signal_name}")
-        return
+    def logg_csv(self):
+        if not self.curve:
+            print("No curve to log.")
+            return
 
-    file_path=os.path.join(directory,f"{signal_name}.csv")
+        try:
+            x_data, y_data = self.curve.getData()
+            if x_data is None or y_data is None:
+                print("Curve data is empty.")
+                return
+        except AttributeError:
+            print(f" No such data is available in the curve for signal '{self.signal_name}'")
+            return
 
-    with open(file_path,'w',newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        headers=["Time (s)","Amplitude"]
-        csv_writer.writerow(headers)
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
 
-        for x, y in zip(x_data, y_data):
-            csv_writer.writerow([x, y])
+        file_path = os.path.join(self.directory, f"{self.signal_name}.csv")
+        with open(file_path, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for x, y in zip(x_data, y_data):
+                writer.writerow([x, y])
 
-        print(f"✅ Logged '{signal_name}' to {file_path}")
+        print(f"✅ Logged '{self.signal_name}' to {file_path}")
 
-    pass
-
-def logg_binary():
-    pass
+    def logg_binary(self):
+        # Placeholder
+        pass
