f5892036ac1c4de073b02bf4e4b8353ecfb6c3a1
farza <farzanrichu610@gmail.com>
2025-07-09 01:53:53 +0530

    added x range selector in the dialogue box

--- graph_plotting_functionalities/AxisRangeDialog.py
+++ graph_plotting_functionalities/AxisRangeDialog.py
@@ -1,2 +1,38 @@
-# axis_range_dialog.py
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QLabel, QMessageBox
+from PyQt5.QtWidgets import (
+    QDialog, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit,
+    QPushButton, QGroupBox, QMessageBox
+)
+
+
+def create_label_input_row(pairs,label_width=90, spacing=10):
+    row = QHBoxLayout()
+    row.setSpacing(spacing)
+    for label, input_line_edit in pairs:
+        label = QLabel(label)
+        label.setFixedWidth(label_width)
+        row.addWidget(label)
+        row.addWidget(input_line_edit)
+    return row
+
+
+class AxisRangeDialog(QDialog):
+    def __init__(self, x_range_current=(0, 10), y_range_current=(-1, 1), parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Set Axis Range")
+        self.setFixedSize(400, 300)
+
+        main_layout = QVBoxLayout()
+
+        # === X Axis Group ===
+        x_box = QGroupBox("X Axis Range")
+        x_layout = QVBoxLayout()
+
+        self.input_x_min = QLineEdit()
+        self.input_x_min.setPlaceholderText("eg:-500")
+        self.input_x_max = QLineEdit()
+        self.input_x_max.setPlaceholderText(" eg:500")
+
+        label_input_row=create_label_input_row([
+            ("Minimum X", self.input_x_min),("Maximum X", self.input_x_max)
+        ])
+        x_layout.addLayout(label_input_row)
